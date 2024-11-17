18f0b99289e43bbf8035424cc33250b6c66a48be
xsimple1010 <aderv78@gmail.com>
2024-11-17 16:37:21 -0300

    add testes em retro_stack.rs

--- src/retro_stack.rs
+++ src/retro_stack.rs
@@ -1,15 +1,14 @@
-use std::sync::{Arc, Mutex};
-
 use retro_ab::paths::Paths;
 use retro_ab_gamepad::devices_manager::Device;
+use std::sync::{Arc, Mutex, MutexGuard};
 
 //facilita o reconhecimento dos atributos usando a intellisense da ide
 type CorePath = String;
 type RomPath = String;
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum StackCommand {
-    //core, rom, paths, callbacks
+    //core, rom, paths
     LoadGame(CorePath, RomPath, Paths),
     SaveState,
     LoadState,
@@ -21,35 +20,75 @@ pub enum StackCommand {
 }
 
 pub struct RetroStack {
-    command: Mutex<Vec<StackCommand>>,
+    commands: Mutex<Vec<StackCommand>>,
 }
 
 impl RetroStack {
     pub fn new() -> Arc<RetroStack> {
         Arc::new(Self {
-            command: Mutex::new(Vec::new()),
+            commands: Mutex::new(Vec::new()),
+        })
+    }
+
+    fn get_commands_mutex(&self) -> MutexGuard<'_, Vec<StackCommand>> {
+        self.commands.lock().unwrap_or_else(|op| {
+            let mut commands = op.into_inner();
+            *commands = Vec::new();
+            commands
         })
     }
 
     pub fn push(&self, command: StackCommand) {
-        self.command.lock().unwrap().push(command);
+        self.get_commands_mutex().push(command);
     }
 
     pub fn read(&self) -> Vec<StackCommand> {
-        let m_cmd = &mut *self.command.lock().unwrap();
-        let cmd = m_cmd.clone().to_vec();
+        let mut commands = self.get_commands_mutex();
+        let v_commands = commands.clone().to_vec();
 
-        m_cmd.clear();
+        commands.clear();
 
-        cmd
+        v_commands
     }
 
     pub fn clear(&self) {
-        match self.command.lock() {
-            Ok(mut cmd) => {
-                cmd.clear();
-            }
-            Err(_e) => {}
-        }
+        self.get_commands_mutex().clear();
+    }
+}
+
+#[cfg(test)]
+mod retro_stack_test {
+    use super::{RetroStack, StackCommand};
+    use std::thread;
+
+    #[test]
+    fn clear() {
+        let stack = RetroStack::new();
+
+        stack.push(StackCommand::Quit);
+
+        let stack_2 = stack.clone();
+
+        let _ = thread::spawn(move || {
+            let commands = stack_2.read();
+            assert_eq!(commands.len(), 1);
+
+            let cmd = commands.first().unwrap().clone();
+            assert_eq!(cmd, StackCommand::Quit)
+        })
+        .join();
+
+        assert_eq!(stack.read().is_empty(), true);
+    }
+
+    #[test]
+    fn push_and_read() {
+        let stack = RetroStack::new();
+
+        stack.push(StackCommand::Quit);
+
+        let commands = stack.read();
+        assert_eq!(commands.len(), 1);
+        assert_eq!(commands.first().unwrap().clone(), StackCommand::Quit);
     }
 }
