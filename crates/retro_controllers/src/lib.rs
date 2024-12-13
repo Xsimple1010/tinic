1fe66dcfc58e7b6f9497cda7f57b4500045dc220
Xsimple1010 <95971568+Xsimple1010@users.noreply.github.com>
2024-12-13 12:53:15 -0300

    pub use GamepadKeyMap

--- crates/retro_controllers/src/lib.rs
+++ crates/retro_controllers/src/lib.rs
@@ -8,6 +8,7 @@ mod keyboard;
 mod retro_controller;
 mod state_thread;
 
+pub use gamepad::gamepad_key_map::GamepadKeyMap;
 pub mod devices_manager;
 pub use retro_controller::{
     input_poll_callback, input_state_callback, rumble_callback, RetroController,
